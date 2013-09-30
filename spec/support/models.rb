267d9fb2d9d84db33c9d14d159b98c58f4da47aa
Magne Land <magne@rightscale.com>
2013-09-30 15:52:25 -0700

    acu119953 be explicit about using attributor types in models spec

--- spec/support/models.rb
+++ spec/support/models.rb
@@ -1,8 +1,8 @@
 class Chicken
   include Attributor::Model
   attributes(:identity => :email) do
-    attribute 'age', Integer, :default => 1, :min => 0, :max => 120, :description => "The age of the chicken"
-    attribute 'email', String, :example => /[:email:]/, :regexp => /@/, :description => "The email address of the chicken"
+    attribute 'age', Attributor::Integer, :default => 1, :min => 0, :max => 120, :description => "The age of the chicken"
+    attribute 'email', Attributor::String, :example => /[:email:]/, :regexp => /@/, :description => "The email address of the chicken"
     attribute 'angry', Attributor::Boolean, :example => "true", :description => "Angry bird?"
   end
 end
@@ -11,9 +11,9 @@ end
 class Duck
   include Attributor::Model
   attributes do
-    attribute 'age', Integer, :required_if => {"name" => "Daffy" }
-    attribute 'name', String 
-    attribute 'email', String, :required_if => "name"
+    attribute 'age', Attributor::Integer, :required_if => {"name" => "Daffy" }
+    attribute 'name', Attributor::String
+    attribute 'email', Attributor::String, :required_if => "name"
     attribute 'angry', Attributor::Boolean, :example => /true|false/, :description => "Angry bird?"
   end
 end
@@ -22,8 +22,8 @@ end
 class Turkey
   include Attributor::Model
   attributes(:identity => :email) do
-    attribute 'age', Integer, :default => 1, :min => 0, :max => 120, :description => "The age of the chicken"
-    attribute 'email', String, :example => /[:email:]/, :regexp => /@/, :description => "The email address of the chicken"
+    attribute 'age', Attributor::Integer, :default => 1, :min => 0, :max => 120, :description => "The age of the chicken"
+    attribute 'email', Attributor::String, :example => /[:email:]/, :regexp => /@/, :description => "The email address of the chicken"
   end
 end
 
