feaa59adb9b731e245e3095f160a09a0263c4897
Magne Land <magne@rightscale.com>
2013-10-04 15:56:59 -0700

    acu119953 must require 'date' to guarantee we are using the std lib DateTime class
    actually this is the real bug fix; ignore previous commit

--- lib/attributor/types/date_time.rb
+++ lib/attributor/types/date_time.rb
@@ -1,18 +1,14 @@
 # Represents a plain old boolean type. TBD: can be nil?
 #
 require_relative '../exceptions'
+require 'date'
 
 module Attributor
 
     class DateTime
       include Type
 
-      # Note: cannot use DateTime.new - causes error in some rubies
-      # Example error:
-      # ruby 1.9.2p290 (2011-07-09 revision 32553) [x86_64-linux]
-      # Linux ci-linux-1.test.rightscale.com 2.6.32-345-ec2 #49-Ubuntu SMP Fri May 25 09:50:04 UTC 2012 x86_64 GNU/Linux
-      # INTERNAL ERROR!!! undefined method `[]' for nil:NilClass
-      ATTRIBUTOR_EPOCH = ::DateTime.strptime("9/30/2013 8:00 AM", "%m/%d/%Y %H:%M %p")
+      ATTRIBUTOR_EPOCH = ::DateTime.new(2013, 9, 30, 8, 0, 0)
 
       def self.native_type
         return ::DateTime
