93e0fe741f6062fcf32290f2ebfe4b8407189e86
Justin Gaylor <justin@rightscale.com>
2014-12-05 16:20:47 -0800

    CSV should also dump String values

--- lib/attributor/exceptions.rb
+++ lib/attributor/exceptions.rb
@@ -4,9 +4,9 @@ module Attributor
 
   class LoadError < AttributorException
   end
-  
+
   class IncompatibleTypeError < LoadError
-    
+
     def initialize(type:, value_type: , context: )
       super "Type #{type} cannot load values of type #{value_type} while loading #{Attributor.humanize_context(context)}."
     end
@@ -19,18 +19,18 @@ module Attributor
       super msg
     end
   end
-  
+
   class DeserializationError < LoadError
     def initialize( context: , from:, encoding: , value: nil)
       msg = "Error deserializing a #{from} using #{encoding} while loading #{Attributor.humanize_context(context)}."
       msg += " Received value #{Attributor.errorize_value(value)}" if value
       super msg
-    end  
+    end
   end
-  
+
   class DumpError < AttributorException
     def initialize( context: , name: , type: , original_exception: )
-      msg = "Error while dumping attribute #{name} of type #{type} for context #{Attributor.humanize_context(context)} ."
+      msg = "Error while dumping attribute #{name} of type #{type} for context #{Attributor.humanize_context(context)}."
       msg << " Reason: #{original_exception}"
       super msg
     end
--- lib/attributor/types/csv.rb
+++ lib/attributor/types/csv.rb
@@ -6,6 +6,22 @@ module Attributor
       value.split(',')
     end
 
+    def self.dump(values, **opts)
+      case values
+      when ::String
+        values
+      when ::Array
+        values.collect { |value| member_attribute.dump(value,opts).to_s }.join(',')
+      else
+        context = opts[:context]
+        name =  opts[:context].last.to_s
+        type = values.class.name
+        reason = "Attributor::CSV only supports dumping values of type " +
+                 "Array or String, not #{values.class.name}."
+        raise DumpError.new(context: context, name: name, type: type, original_exception: reason)
+      end
+    end
+
     def self.example(context=nil, options: {})
       collection = []
       while collection.size < 2
