bb10d489e4b748fb73fd0172dee4c722cb129adc
Dane Jensen <dane@rightscale.com>
2015-12-01 09:51:42 -0800

    Change FieldSelector to use {}s instead of ()s

--- lib/attributor/extras/field_selector/parser.rb
+++ lib/attributor/extras/field_selector/parser.rb
@@ -3,7 +3,7 @@ module Attributor
     class Parser < Parslet::Parser
       rule(:simple_name) { match('[a-zA-Z0-9_]').repeat(1) }
       rule(:item) { simple_name.as(:field) >> parenthesized.repeat(0).as(:children) }
-      rule(:parenthesized) { str('(') >> csv >> str(')') }
+      rule(:parenthesized) { str('{') >> csv >> str('}') }
       rule(:csv) { (item >> (str(',') >> item).repeat(0)).as(:csv) }
       root(:csv)
     end
