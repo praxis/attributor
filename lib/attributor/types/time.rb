4ddf474ab2f688966ba3ebde73d25f2346ad53c8
Josep M. Blanquer <blanquer@rightscale.com>
2016-05-17 11:21:07 -0700

    `.example` method signature for `_options` kw
    Fixed the `.example` method signature after the previous refactoring of a couple days ago.
    Some types (symbol, temple and time) had incorrectly been refactored to have a kw argument called `_options`
    
    Signed-off-by: Josep M. Blanquer <blanquer@rightscale.com>

--- lib/attributor/types/time.rb
+++ lib/attributor/types/time.rb
@@ -8,7 +8,7 @@ module Attributor
       ::Time
     end
 
-    def self.example(context = nil, _options: {})
+    def self.example(context = nil, **_options)
       load(Randgen.time, context)
     end
 
