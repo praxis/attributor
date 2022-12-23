4bd6befd539fa9a9467ebd40258da6c8878ee381
Josep M. Blanquer <blanquer@C02F20XQMD6W.local>
2022-12-23 13:45:15 +0100

    bigDecimal cannot be represented as a number in Json Schema ... so we represent it as a string, with a 'format' of "bigdecimal" since I don't know what other name to give it
    * maybe arbitrary precision?

--- lib/attributor/types/bigdecimal.rb
+++ lib/attributor/types/bigdecimal.rb
@@ -19,7 +19,18 @@ module Attributor
     end
 
     def self.json_schema_type
-      :number
+      :string
+    end
+
+    # Bigdecimal numbers are too big to be represented as numbers in JSON schema
+    # The way to do so, is to represent them as strings, but add a 'format' specifying
+    # how to parse that (seemingly, there is a 'double' well known type that json API 
+    # claims we can use) ... not sure if this is the right one, as technically BigDecimal
+    # has very large precision that a double wouldn't be able to fit...
+    def self.as_json_schema( shallow: false, example: nil, attribute_options: {} )
+      hash = super
+      hash[:format] = 'bigdecimal'
+      hash
     end
   end
 end
