06ea2bf5c1e51fa0cfeacdbc8ffcddcb663f9dfb
Danial Moazen <gdanialq@gmail.com>
2023-06-09 09:14:40 -0700

    dm - Float should render string as float

--- lib/attributor/types/float.rb
+++ lib/attributor/types/float.rb
@@ -18,7 +18,7 @@ module Attributor
     end
 
     def self.load(value, context = Attributor::DEFAULT_ROOT_CONTEXT, **options)
-      return BigDecimal(value + '0') if value.is_a?(::String) && value.end_with?('.')
+      return Float(value + '0') if value.is_a?(::String) && value.end_with?('.')
       Float(value)
     rescue TypeError
       super
